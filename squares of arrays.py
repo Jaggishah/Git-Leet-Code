9c2ebac98bc93570f5f1719a30a0fa8116dc4a36
Jaggi shah <jaggishah@gmail.com>
2022-08-27 00:29:37 -0700

    squares

---
+++ squares of arrays.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i]=nums[i]*nums[i]
+        nums = sorted(nums)
+        return nums
+            
+        
+            
+            
+        
