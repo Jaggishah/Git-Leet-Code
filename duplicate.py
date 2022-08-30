322d05a32f77a46497bd2fd3c358d30ec4056108
Jaggi shah <jaggishah@gmail.com>
2022-08-30 03:49:04 -0700

    duplicate

--- duplicate.py
+++ duplicate.py
@@ -1,21 +1,25 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        a = len(nums)-1
-        f = 0
-        o = 1
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         a = len(nums)-1
+#         f = 0
+#         o = 1
         
         
         
         
-        for i in range(1,len(nums)):
-            if nums[i]!=nums[i-1]:
-                nums[o]=nums[i]
-                o+=1
+#         for i in range(1,len(nums)):
+#             if nums[i]!=nums[i-1]:
+#                 nums[o]=nums[i]
+#                 o+=1
                 
             
                 
-        return o
-            
-            
+#         return o
+
+from re import A
+
+
+in = A
+print(in)
             
                 
